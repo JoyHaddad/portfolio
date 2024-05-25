fa2be6d23bbddd62ee08a181da1225e5e7104bd5
JoyHaddad <joy4haddad@gmail.com>
2024-05-24 21:33:33 -0700

    add profile and title

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,14 @@
 import "@/styles/globals.css";
+import Head from "next/head";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <link rel="icon" href="/profile.jpg" type="image/jpg" />
+        <title>Joy Haddad</title>
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
--- pages/index.js
+++ pages/index.js
@@ -10,68 +10,70 @@ import { motion } from "framer-motion";
 
 export default function Home() {
   return (
-    <div id="hero">
-      <Navbar />
-      <Welcome />
-      <div className="h-screen pt-16">
-        <motion.div
-          initial={{ x: -1000 }}
-          whileInView={{ x: 0 }}
-          transition={{
-            type: "spring",
-            stiffness: 120,
-            duration: 0.5,
-            delay: 0.1,
-          }}
-          viewport={{ once: true }}
-          className="text-4xl mx-0 font-bold mb-10 sm:mx-32"
-          id="projects"
-        >
-          Projects
-        </motion.div>
-        <div className="mx-0 sm:mx-32 flex sm:flex-row flex-col justify-between">
-          <Projects
-            projectImage={snapChefImg}
-            project={{
-              title: "SnapChef",
-              description:
-                "Social media web app dedicated to sharing and discovering recipes!",
-              tag1: "Next.js",
-              tag2: "MongoDB",
-              tag3: "Tailwind",
-              projectImage: "test",
-              link: "https://snapchefapp.vercel.app/",
+    <div>
+      <div id="hero">
+        <Navbar />
+        <Welcome />
+        <div className="h-screen pt-16">
+          <motion.div
+            initial={{ x: -1000 }}
+            whileInView={{ x: 0 }}
+            transition={{
+              type: "spring",
+              stiffness: 120,
+              duration: 0.5,
+              delay: 0.1,
             }}
-          />
-          <Projects
-            projectImage={fitnessImg}
-            project={{
-              title: "Fitness Tracker",
-              description:
-                "Android/ios app for creating workout plans and tracking exercise PR!",
-              tag1: "ReactNative",
-              tag2: "SQL",
-              tag3: "FastAPI",
-              link: "https://github.com/ale18V/Fitcker",
-            }}
-          />
-          <Projects
-            projectImage={mediSearchImg}
-            project={{
-              title: "MediSearch",
-              description:
-                "Search engine for any medication details and warnings listed from the FDA!",
-              tag1: "Next.js",
-              tag2: "OpenFDA",
-              tag3: "Tailwind",
-              link: "https://medisearchapp.vercel.app/",
-            }}
-          />
+            viewport={{ once: true }}
+            className="text-4xl mx-0 font-bold mb-10 sm:mx-32"
+            id="projects"
+          >
+            Projects
+          </motion.div>
+          <div className="mx-0 sm:mx-32 flex sm:flex-row flex-col justify-between">
+            <Projects
+              projectImage={snapChefImg}
+              project={{
+                title: "SnapChef",
+                description:
+                  "Social media web app dedicated to sharing and discovering recipes!",
+                tag1: "Next.js",
+                tag2: "MongoDB",
+                tag3: "Tailwind",
+                projectImage: "test",
+                link: "https://snapchefapp.vercel.app/",
+              }}
+            />
+            <Projects
+              projectImage={fitnessImg}
+              project={{
+                title: "Fitness Tracker",
+                description:
+                  "Android/ios app for creating workout plans and tracking exercise PR!",
+                tag1: "ReactNative",
+                tag2: "SQL",
+                tag3: "FastAPI",
+                link: "https://github.com/ale18V/Fitcker",
+              }}
+            />
+            <Projects
+              projectImage={mediSearchImg}
+              project={{
+                title: "MediSearch",
+                description:
+                  "Search engine for any medication details and warnings listed from the FDA!",
+                tag1: "Next.js",
+                tag2: "OpenFDA",
+                tag3: "Tailwind",
+                link: "https://medisearchapp.vercel.app/",
+              }}
+            />
+          </div>
         </div>
+        <div></div>
+        <About />
+        <Footer />
       </div>
-      <div></div>
-      <About />
-      <Footer />
     </div>
   );
 }
